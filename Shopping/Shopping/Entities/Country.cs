32f306c750b73872930579cf3e5292d48133c420
merestrepo <merestrepo@DESKTOP-TFAA3KQ>
2022-10-04 14:18:47 -0500

    CountriesController

---
+++ Shopping/Shopping/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name= "Pais")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo 50 caracteres")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}
