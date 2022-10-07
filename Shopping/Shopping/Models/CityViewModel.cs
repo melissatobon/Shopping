9fd0a9d51ad861ec13cd420b4bc94f17f32696a6
merestrepo <merestrepo@DESKTOP-TFAA3KQ>
2022-10-07 09:29:02 -0500

    FINISH CRUD STATES AND CITIES

---
+++ Shopping/Shopping/Models/CityViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Shopping.Models
+{
+    public class CityViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Ciudad")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo 50 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+        public int StateId { get; set; }
+    }
+}
