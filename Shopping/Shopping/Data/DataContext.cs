06b78f91d59ce4fd5d82bf88ccc96e10bab995b0
merestrepo <merestrepo@DESKTOP-TFAA3KQ>
2022-10-19 15:36:56 -0500

    shopping cart

--- Shopping/Shopping/Data/DataContext.cs
+++ Shopping/Shopping/Data/DataContext.cs
@@ -18,6 +18,7 @@ namespace Shopping.Data
         public DbSet<ProductCategory> ProductCategories { get; set; }
 
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<TemporalSale> TemporalSales { get; set; }
 
         public DbSet<State> States { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
