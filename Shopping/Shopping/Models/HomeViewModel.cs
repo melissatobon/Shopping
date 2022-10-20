c97ab0658c293b72e9f5c382d811b7c0d1dd94d4
merestrepo <merestrepo@DESKTOP-TFAA3KQ>
2022-10-20 12:17:36 -0500

    Order, filter, pagination in home

--- Shopping/Shopping/Models/HomeViewModel.cs
+++ Shopping/Shopping/Models/HomeViewModel.cs
@@ -1,11 +1,13 @@
-﻿using Shopping.Entities;
+﻿using Shopping.Common;
+using Shopping.Entities;
 
 namespace Shopping.Models
 {
     public class HomeViewModel
     {
         public float Quantity { get; set; }
-        public ICollection<Product> Products { get; set; }
+        public PaginatedList<Product> Products { get; set; }
+        public ICollection<Category> Categories { get; set; }
 
     }
 }
